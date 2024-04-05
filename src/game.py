a6cfbc0468b1a213de0ca959617c0127da061d93
Jay Barrett <jab864@cornell.edu>
2024-04-05 19:18:36 -0400

    Implemented event handling in game logic
    - game.py has handle_events function

--- src/game.py
+++ src/game.py
@@ -13,6 +13,19 @@ class Game:
         self.asteroids = []
         self.bullets = []
 
+    def handle_events(self):
+        for event in pygame.event.get():
+            self.running = False
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            self.player.rotate_left()
+        if keys[pygame.K_RIGHT]:
+            self.player.rotate_right()
+        if keys[pygame.K_UP]:
+            self.player.accelerate()
+        if keys[pygame.K_SPACE]:
+            self.player.shoot(self.bullets)
+
     def run(self):
         while self.running:
             self.handle_events()
@@ -36,3 +49,10 @@ class Game:
         self.player.draw()
         # Draw asteroids and bullets
         pygame.display.flip()
+
+    def draw_ui(self):
+        ammo_count = 20 - len([b for b in self.bullets if b.is_alive()])
+        ammo_text = f"Ammo: {ammo_count}"
+        font = pygame.font.SysFont(None, 36)
+        text_surf = font.render(ammo_text, True, settings.WHITE)
+        self.screen.blit(text_surf, (10, self.scree.get_height() - 40))
